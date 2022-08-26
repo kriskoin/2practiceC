7adc9a987621a439e75f411f46d85679ba2eadf0
kriskoin <cris7775@gmail.com>
2022-08-26 17:21:23 -0600

    add gitignore file

---
+++ README.md
@@ -0,0 +1,4 @@
+# 2practiceC
+## binary search
+Example code for binary search
+the search is made on a sorted array
---
+++ program.cpp
@@ -0,0 +1,45 @@
+/*
+how to compile:
+mkdir build
+cd build
+cmake ..
+make
+
+ how to run
+ cd build
+./program
+
+*/
+#include <vector>
+#include <iostream>
+
+static int binarySearch(const std::vector<int>& sortedVector, int lessThan){
+   
+    if (!((int )sortedVector.size())){
+        return 0;
+    }
+
+    if (lessThan<sortedVector[0]){
+        return 0;
+    }
+
+    int vecSize = (int)sortedVector.size();
+
+    if (sortedVector[vecSize-1]<lessThan){
+        return vecSize;
+    }
+/*
+    int s = 0;
+    int f = (int)sortedVector.size() /2 ;
+    while (s<f){
+
+    }*/
+    return 0;
+}
+
+int main(){
+    
+    std::vector<int> v { 12, 23, 50, 70 };
+    std::cout << binarySearch(v, 35)<<std::endl;
+    return 0;
+}
