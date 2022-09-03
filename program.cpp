d6d65637e1a5335dc3cfcbc6ef756ada6d07021a
kriskoin <cris7775@gmail.com>
2022-09-03 14:08:14 -0600

    wip

--- program.cpp
+++ program.cpp
@@ -1,33 +1,65 @@
-/*
-how to compile:
-mkdir build
-cd build
-cmake ..
-make
 
- how to run
- cd build
-./program
-
-hot to test
-cd build
-ctest
-
-*/
 
 #include <vector>
+#include <stdexcept>
 #include <iostream>
-#include <assert.h>
+#include <map>
 
-#ifndef __ALGO_H
-#include "algo.h"
-#endif
+class MovingTotal
+{
+public:
 
-int main(){
-    CBinaryTreeSearch bts;
-    CNode * root = new CNode(nullptr,200,nullptr);
-    binaryTreeAddNode(root,20);
-    assert(true == bts.find(root,200));
-    assert(true == bts.find(root,20));
-    return 0;
+    void append(const std::vector<int>& list) 
+    {
+        std::map<std::vector<int>::iterator it;
+        it=listTotals.find(list);
+        if(it==listTotals.end()){
+            //new list
+            std::vector<int> totals
+            ret = mymap.insert ( std::vector<int> *, std::vector<int>(list,totals) );
+            calculateTotals(ret->first,ret->second);
+        }else{
+            //old list
+
+        }
+    }
+    
+    bool contains(int total) 
+    {
+        throw std::logic_error("Waiting to be implemented");
+    }
+    private:
+    void calculateTotals(std::vector<int> *, std::vector<int>){
+
+    } 
+    std::map<std::vector<int> *, std::vector<int>> listTotals;
+};
+
+#ifndef RunTests
+int main()
+{
+    MovingTotal movingTotal;
+
+    std::vector<int> first;
+    first.push_back(1);
+    first.push_back(2);
+    first.push_back(3);
+    first.push_back(4);
+
+    movingTotal.append(first);
+
+    std::cout << movingTotal.contains(6) << std::endl;
+    std::cout << movingTotal.contains(9) << std::endl;
+    std::cout << movingTotal.contains(12) << std::endl;
+    std::cout << movingTotal.contains(7) << std::endl;
+
+    std::vector<int> second;
+    second.push_back(5);
+    movingTotal.append(second);
+
+    std::cout << movingTotal.contains(6) << std::endl;
+    std::cout << movingTotal.contains(9) << std::endl;
+    std::cout << movingTotal.contains(12) << std::endl;
+    std::cout << movingTotal.contains(7) << std::endl;
 }
+#endif
